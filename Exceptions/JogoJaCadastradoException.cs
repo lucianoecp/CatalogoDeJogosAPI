2983f3f3ee43d4f725d7ca18152e298e3ef06ce4
Luciano S. de Arruda <71555478+lucianoecp@users.noreply.github.com>
2021-09-13 15:52:28 -0300

    up da previa no git

---
+++ Exceptions/JogoJaCadastradoException.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CatalogoDeJogosAPI.Exceptions
+{
+    public class JogoJaCadastradoException : Exception
+    {
+        public JogoJaCadastradoException()
+            : base("Este já jogo está cadastrado")
+        { }
+    }
+}
